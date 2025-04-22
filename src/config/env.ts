f5868e6283025cc37506483d1f89e63b8dca47ca
Mazen Badr <mazen@datamilk.app>
2025-04-22 13:27:17 +0200

    remove unit test files.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,12 +1,18 @@
+import dotenv from "dotenv";
 import { z } from "zod";
 
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: ".env.local" });
+}
+
 const envSchema = z.object({
-  OPENAI_API_KEY: z.string(),
-  GCP_PROJECT_ID: z.string(),
-  USE_GCS: z.string(),
-  CSV_BUCKET: z.string(),
-  USE_AUTH: z.string(),
-  API_SECRET_TOKEN: z.string(),
+  OPENAI_API_KEY: z.string().min(1, "Missing OpenAI API key"),
+  GCP_PROJECT_ID: z.string().min(1, "Missing GCP project Id"),
+  USE_GCS: z.string().min(1, "Missing GCS Usage Flag"),
+  CSV_BUCKET: z.string().min(1, "Missing GCS Input Path"),
+  USE_AUTH: z.string().min(1, "Missing Use AUTH Flag"),
+  API_SECRET_TOKEN: z.string().min(1, "Missing API Secret Token"),
+  PORT: z.string().optional(),
 });
 
 const parsed = envSchema.safeParse(process.env);
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import { mindMapRouter } from "./api/mindMapApi";
 import { authenticateToken } from "./middleware/authMiddleWare";
+import { env } from "./config/env";
 
 const app = express();
-const port = Number(process.env.PORT) || 8080;
+const port = Number(process.env.PORT || env.PORT) || 8080;
 
 app.use(cors());
 app.use(express.json());
