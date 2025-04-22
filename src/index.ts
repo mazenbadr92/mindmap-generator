8c4dcd0374b0c98b88c4b5a2182dff253914869b
Mazen Badr <mazen@datamilk.app>
2025-04-22 14:54:26 +0200

    fixing index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import { mindMapRouter } from "./api/mindMapApi";
 import { authenticateToken } from "./middleware/authMiddleWare";
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = Number(process.env.PORT) || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use("/", authenticateToken, mindMapRouter);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
 });
