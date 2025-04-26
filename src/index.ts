73debb01936105544ff4e747584dd4bcb2401270
Mazen Badr <mazen@datamilk.app>
2025-04-26 07:33:53 +0300

    removing embedded swagger

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import { mindMapRouter } from "./api/mindMapApi";
 import { authenticateToken } from "./middleware/authMiddleWare";
 import { StatusCodes } from "http-status-codes";
-import { swaggerDocHandler, swaggerUiHandler } from "./swagger";
 
 const app = express();
 const port = Number(process.env.PORT) || 8080;
@@ -11,8 +10,6 @@ const port = Number(process.env.PORT) || 8080;
 app.use(cors());
 app.use(express.json());
 app.use("/", authenticateToken, mindMapRouter);
-
-app.use("/docs", swaggerUiHandler, swaggerDocHandler);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server is running on http://localhost:${port}`);
